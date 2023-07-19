e635db8eb48a8d23e1c215bffe941fa46b5cea7d
raulrsantos <raulramosdossantos1@gmail.com>
2023-07-18 22:00:32 -0300

    Visual
    Alterações de visual

--- resources/views/produtos/detail.blade.php
+++ resources/views/produtos/detail.blade.php
@@ -1,8 +1,10 @@
 @extends('website/website')
 
 @section('content')
-    <div>
-        <h1>{{ $produto->name }}</h1>
+<div class="container px-4 py-5" id="custom-cards">
+    <h1 class="pb-2 border-bottom">{{ $produto->name }}</h1>
+    <div class="row py-5">
         <p>{{ $produto->detail }}</p>
     </div>
+</div>
 @endsection
--- resources/views/produtos/home.blade.php
+++ resources/views/produtos/home.blade.php
@@ -1,13 +1,23 @@
 @extends('website/website')
 
 @section('content')
-    <div class=>
-        @foreach($produtos as $item)
-            <div>
-                <a href="{{ url('/'. $item->id)}}">
-                    {{ $item->name }}
-                </a>
+    
+    <div class="container px-4 py-5" id="custom-cards">
+    <h2 class="pb-2 border-bottom">Produtos</h2>
+
+    <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
+    @foreach($produtos as $item)
+
+        <a href="{{ url('/'. $item->id)}}" style="decoration: none !important;">
+            <div class="col">
+                <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg" style="background-image: url('unsplash-photo-3.jpg');">
+                    <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                        <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">{{ $item->name }}</h3>
+                    </div>
+                </div>
             </div>
-        @endforeach
+        </a>
+      @endforeach
     </div>
+</div>
 @endsection 
